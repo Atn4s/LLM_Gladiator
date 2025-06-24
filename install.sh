46ea122aa399d55683b963f1ff69df058c9c62aa
Atn4s <joaopsantaclara115@gmail.com>
2025-06-24 20:58:54 -0300

    v 1.2 - nova versão

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script robusto para instalar e testar PaddleOCR com fallback automático para paddlepaddle==2.6.2
+# Autor: Atn4s com ajuda da IA 👾
+clear
+echo "🚀 Iniciando setup do LLM_Gladiator!"
+
+# 1. Cria e ativa ambiente virtual
+echo "🐍 Criando ambiente virtual 'LLM_Gladiator' na pasta atual"
+python3.12 -m venv LLM_Gladiator
+source LLM_Gladiator/bin/activate
+
+# 2 Modifica a estrutura do PATH para que o LLM_Gladiator fique organizado
+echo "🔧 Movendo diretórios para dentro do PaddleOCR e instalando o restante das dependencias!"
+    mv llm_wrappers/ LLM_Gladiator/
+    mv limpeza_json.py LLM_Gladiator/
+    mv LLM_Gladiator.py LLM_Gladiator/     
+    pip install -r requirements.txt        
+    mv requirements.txt LLM_Gladiator/    
+    cd LLM_Gladiator/     
+
+echo "🏁 Setup do LLM_Gladiator foi finalizado!"
