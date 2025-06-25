8a41964c78c5ca3b0fd277ef15e9c27a3d56c6f4
Atn4s <joaopsantaclara115@gmail.com>
2025-06-24 23:32:57 -0300

    v 1.3 - stable

--- processa_LLM.sh
+++ processa_LLM.sh
@@ -2,6 +2,8 @@
 # Script para processar imagens com LLM_Gladiator
 # Autor: Atn4s com ajuda da IA 👾
 clear
+source bin/activate
+
 echo "🚀 Iniciando processamento de imagens com LLM_Gladiator!"
 python3 LLM_Gladiator.py 
 echo "🏁 Processamento concluído! Agora a formatação do JSON será realizada"
